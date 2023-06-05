52068994aa8957f32373bc031bd15a27d7f51060
Vanessa Silva <vanessamikaelly66@gmail.com>
2023-06-05 18:42:14 -0300

    first commit

---
+++ TryEcatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+try
+{
+
+    Console.WriteLine("Informe o primeiro valor: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o segundo valor: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int result = a / b;
+
+    Console.WriteLine($"O resultado é {result}");
+
+}
+
+
+catch(DivideByZeroException e) //expecifico divisao por zero
+{
+    Console.WriteLine($"Não é permitido dividir por zero " + e.Message); //imprimindo a mensagem de erro
+}
+
+catch(FormatException e)
+{
+    Console.WriteLine($"Conversão de string para letra não é permitida! { e.Message}");
+}
+
+catch(Exception e) //geral, tem que estar por ultimo
+{
+    Console.WriteLine($"Ocorreu um erro {e.Message}");
+}
+
+finally     //independente se deu certo ou errado vai passar no finally
+{
+    Console.WriteLine("Aqui passa de qualquer forma!");
+}
+
+
+
+Console.ReadKey();
